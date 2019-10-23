a68268ebd24851104201e8b24467ae9f2001e7b4
UncleBacon <47467787+UncleBacon@users.noreply.github.com>
2019-10-23 12:19:24 -0400

    fixed questions 6 & 7

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,71 +1,3 @@
-
-
-CREATE TABLE Departments (
-    dept_no varchar   NOT NULL,
-    dept_name varchar   NOT NULL,
-    CONSTRAINT pk_Departments PRIMARY KEY (
-        dept_no
-     )
-);
-
-CREATE TABLE employees (
-    emp_no int   NOT NULL,
-    birth_date date   NOT NULL,
-    first_name varchar   NOT NULL,
-    last_name varchar   NOT NULL,
-    gender varchar   NOT NULL,
-    hire_date date   NOT NULL,
-    CONSTRAINT pk_employees PRIMARY KEY (
-        emp_no
-     )
-);
-
-CREATE TABLE dept_emp (
-    emp_no int   NOT NULL,
-    dept_no varchar   NOT NULL,
-    from_date date   NOT NULL,
-    to_date date   NOT NULL
-);
-
-CREATE TABLE dept_manager (
-    dept_no varchar   NOT NULL,
-    emp_no int   NOT NULL,
-    from_date date   NOT NULL,
-    to_date date   NOT NULL
-);
-
-CREATE TABLE salaries (
-    emp_no int   NOT NULL,
-    salary int   NOT NULL,
-    from_date date   NOT NULL,
-    to_date date   NOT NULL
-);
-
-CREATE TABLE titles (
-    emp_no int   NOT NULL,
-    title varchar   NOT NULL,
-    from_date date   NOT NULL,
-    to_date date   NOT NULL
-);
-
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
-
-ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
-ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
-REFERENCES "employees" (emp_no);
-
-ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
-REFERENCES employees (emp_no);
-
 -- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
 select e.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name", e.gender as "Gender", s.salary as "Salary" 
 from employees e
@@ -76,7 +8,7 @@ on (e.emp_no = s.emp_no) limit 10
 select first_name as "First Name", last_name as "Last Name", hire_date as "Hire Date" 
 from employees 
 where hire_date >= '1986-01-01' and hire_date < '1986-12-31'
-order by "Hire Date" limit 12
+order by "Hire Date"
 
 
 -- 3.List the manager of each department with the following information: department number, department name, the 
@@ -104,22 +36,24 @@ and
 last_name like 'B%'
 limit 10
 
+select * from Departments
+
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-select e.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name", d.dept_name as "Department"
-from employees e
-inner join dept_manager  on (dept_manager.emp_no = e.emp_no)
-inner join "Departments" d on (d.dept_no = d.dept_no)
-where d.dept_name = 'Sales'
-limit 10
+select d.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name","Departments".dept_name as "Department"
+from dept_emp as d
+inner join employees as e on (d.emp_no = e.emp_no)
+inner join "Departments" on (d.dept_no = "Departments".dept_no)
+where "Departments".dept_name = 'Sales'
+
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, 
 -- first name, and department name.
-select e.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name", d.dept_name as "Department"
-from employees e
-inner join dept_manager  on (dept_manager.emp_no = e.emp_no)
-inner join "Departments" d on (d.dept_no = d.dept_no)
-where d.dept_name = 'Sales' or d.dept_name = 'Development'
+select d.emp_no as "Employee Number", e.last_name as "Last Name", e.first_name as "First Name","Departments".dept_name as "Department"
+from dept_emp as d
+inner join employees as e on (d.emp_no = e.emp_no)
+inner join "Departments" on (d.dept_no = "Departments".dept_no)
+where "Departments".dept_name = 'Sales' or "Departments".dept_name = 'Development' 
 limit 10
 
 
